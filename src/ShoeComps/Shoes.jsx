bc09b7287364d7029f003702ce408c4773dd49f7
SamsonB25 <SamsonB25@yahoo.com>
2023-06-08 16:54:45 -0500

    added shoe click handler. no functionality yet

--- src/ShoeComps/Shoes.jsx
+++ src/ShoeComps/Shoes.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 const Shoes = () => {
   // setting shoes initial state to an empty array so the map method doesn't error out
   const [shoes, setShoes] = useState([]);
+  const [status, setStatus] = useState("");
 
   // get request from my api to set the initial render of the webpage
   useEffect(() => {
@@ -17,6 +18,11 @@ const Shoes = () => {
     getShoeData();
   }, []);
 
+  const shoeClickHandler = () => {
+    setStatus("shoe clicked");
+    console.log("shoe clicked");
+  };
+
   console.log(shoes.map((shoe) => shoe));
   return (
     <>
@@ -25,21 +31,22 @@ const Shoes = () => {
       <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         {/* map over the shoes array to get individual shoe data*/}
         {shoes.map((shoe) => (
-          <div key={shoe.id} className="shoe-info rounded-md m-2">
-            <div className="shoe-img ">
-              <img
-                className={"rounded-md"}
-                src={shoe.shoe_img[0]}
-                alt={shoe.shoe_name}
-              />
-            </div>
-            <div className="mt-4 flex justify-between">
-              <p>
-                {shoe.shoe_name}
-                <p>{shoe.shoe_price}</p>
+          <div key={shoe.id} className="group">
+            <div
+              className="shoe-info rounded-md m-2"
+              onClick={shoeClickHandler}
+            >
+              <div className="shoe-img aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md lg:aspect-none group-hover:opacity-75 group-hover:cursor-pointer lg:h-50">
+                <img src={shoe.shoe_img[0]} alt={shoe.shoe_name} />
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  {shoe.shoe_name}
+                  <p>{shoe.shoe_price}</p>
 
-                <p>{shoe.shoe_desc}</p>
-              </p>
+                  <p>{shoe.shoe_desc}</p>
+                </div>
+              </div>
             </div>
           </div>
         ))}
